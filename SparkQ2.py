cc737271de75c8e9894fa2293e5582d853a92524
MahiCreator <maheswariarjilli1320@gmail.com>
2026-02-16 22:34:37 +0530

    Update SparkQ2.py
    Add Q2 solution: Time Converter

--- SparkQ2.py
+++ SparkQ2.py
@@ -1,6 +1,6 @@
 def conver_seconds(total_seconds: int) -> str;
- minutes = total_seconds // 60
- seconds = total_seconds % 60
- return f"\"{minutes}m {seconds}s\""
+  minutes = total_seconds // 60
+  seconds = total_seconds % 60
+  return f"\"{minutes}m {seconds}s\""
 total_seconds = int(input("Enter total seconds:"))
 print(convert_seconds(total_seconds))

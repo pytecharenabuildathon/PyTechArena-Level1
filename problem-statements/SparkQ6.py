d412714b05bcfb63575ebfa7b6a2882b01bbb146
MahiCreator <maheswariarjilli1320@gmail.com>
2026-02-17 21:55:11 +0530

    Delete problem-statements/SparkQ6.py

--- problem-statements/SparkQ6.py
+++
@@ -1,4 +0,0 @@
-def convert_temperature(value: float , unit : str) -> float | str:
-    return round((value*9/5)+32,1) if unit.upper()=='C' else \
-           round((value-32)*5/9,1) if unit.upper()=='F' else "Invalid Unit"
-print(convert_temperature(float(input()), input()))
